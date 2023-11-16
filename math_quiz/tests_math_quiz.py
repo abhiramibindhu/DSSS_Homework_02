4ecb8395d96ff3d88bb759f2bc4313fd06d0c82f
Abaamii <150955652+Abaamii@users.noreply.github.com>
2023-11-16 10:42:24 +0100

    Update tests_math_quiz.py

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,5 +1,5 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import function_D, function_E, function_F
 
 
 class TestMathGame(unittest.TestCase):
@@ -7,7 +7,7 @@ class TestMathGame(unittest.TestCase):
     def test_function_D(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
-        max_val = 10
+        max_val = 100
         for _ in range(1000):  # Test a large number of random values
             rand_num = function_D(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
@@ -18,7 +18,7 @@ class TestMathGame(unittest.TestCase):
 
     def test_function_F(self):
             test_cases = [
-                (5, 2, '+', '5 + 2', 7),
+                (3, 4, '+', '3 + 4', 7), (5, 2, '-', '5 - 2', 3), (6, 6, '*', '6 * 6', 36 )
                 ''' TODO add more test cases here '''
             ]
 
